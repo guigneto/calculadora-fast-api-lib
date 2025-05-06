25046d07317ddf5eda8b09bbdd4b7b15e83523bc
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-05-06 16:45:04 -0300

    Add operations in controller
    Apply MVC in operations

---
+++ app/controllers/calculator_controller.py
@@ -0,0 +1,16 @@
+from calculadora_lib import operations
+
+def soma(a: float, b: float) -> float:
+    return operations.soma(a, b)
+
+def subtracao(a: float, b: float) -> float:
+    return operations.subtracao(a, b)
+
+def multiplicacao(a: float, b: float) -> float:
+    return operations.multiplicacao(a, b)
+
+def divisao(a: float, b: float) -> float:
+    return operations.divisao(a, b)
+
+def exponenciacao(a: float, b: float) -> float:
+    return operations.exponenciacao(a, b)
