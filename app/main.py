2e92e3e7e66de0c0eb9aaf3ee81e0b9388f63cbb
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-05-06 16:45:19 -0300

    Apply MVC in main code

--- app/main.py
+++ app/main.py
@@ -1,33 +1,5 @@
-from fastapi import FastAPI, HTTPException
-from app.models import Operacao
-from calculadora_lib import soma, subtracao, multiplicacao, divisao, exponenciacao
+from fastapi import FastAPI
+from app.views.routes import router
 
-app = FastAPI()
-
-@app.post("/soma")
-def rota_soma(valores: Operacao):
-    resultado = soma(valores.a, valores.b)
-    return {"resultado": resultado}
-
-@app.post("/subtracao")
-def rota_subtracao(valores: Operacao):
-    resultado = subtracao(valores.a, valores.b)
-    return {"resultado": resultado}
-
-@app.post("/multiplicacao")
-def rota_multiplicacao(valores: Operacao):
-    resultado = multiplicacao(valores.a, valores.b)
-    return {"resultado": resultado}
-
-@app.post("/divisao")
-def rota_divisao(valores: Operacao):
-    try:
-        resultado = divisao(valores.a, valores.b)
-        return {"resultado": resultado}
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    
-@app.post("/exponenciacao")
-def rota_exponenciacao(valores: Operacao):
-    resultado = exponenciacao(valores.a, valores.b)
-    return {"resultado": resultado}
+app = FastAPI(title="Calculadora MVC")
+app.include_router(router)
